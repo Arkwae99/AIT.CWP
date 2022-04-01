69241af364ce9c357d58f1a3d8ecfe1e67ac0f5b
Aung Phone Khant <33966293+Arkwae99@users.noreply.github.com>
2022-04-01 11:13:01 +0630

    Add files via upload

---
+++ CWP/CWP/Controllers/HelloWorld.cs
@@ -0,0 +1,32 @@
+﻿using CWP.IdentityAuth;
+using CWP.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CWP.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorld : ControllerBase
+    {
+        private readonly ILogger<HelloWorld> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HelloWorld(ILogger<HelloWorld> logger, UserManager<ApplicationUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        [HttpGet(Name = "GetHelloWorld")]
+        public string Get()
+        {
+            return "Hello " + _userManager.GetUserName(User).ToString();
+        }
+
+    }
+}
---
+++ CWP/CWP/IdentityAuth/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CWP.IdentityAuth
+{
+    public class ApplicationUser: IdentityUser
+    {
+    }
+}
